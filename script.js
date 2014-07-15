b47e81e6922c1b7c78b12b208a06a0410d8d91ce
Erich Welz <erichwelz@gmail.com>
2014-07-15 12:24:34 -0400

    adding transition, switching js from faked toggle to built in toggle

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 // console.log("script loaded");
 
 function menuToggle() {
-  var element = document.getElementById('menu-items');
-  element.style.display = (element.style.display == "inherit"
-    ? "none"
-    : "inherit");
+  document.body.classList.toggle("menu");
+
+  // var element = document.getElementById('menu-items');
+  // element.style.display = (element.style.display == "inherit"
+  //   ? "none"
+  //   : "inherit");
 };
 
 window.addEventListener('load', function() {
